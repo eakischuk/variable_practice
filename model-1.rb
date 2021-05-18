8735dbe42e112a0c952c298047704aacc54427b4
Erika Kischuk <erika.kischuk@gmail.com>
2021-05-17 20:26:01 -0600

    Add cat personality hash

--- model-1.rb
+++ model-1.rb
@@ -1,7 +1,7 @@
 name = 'Erika Kischuk'
 hometown = 'Muncie'
-cat = 'asshole'
 cat_age = 8.5
 number_of_cats = 3
 cat_colors = ['Orange', 'Brown', 'Black and White']
 cat_size = ['Tiny', 'Average', 'Chonky']
+cat_personality = {S: asshole, M: geriatric, H: demonic}
